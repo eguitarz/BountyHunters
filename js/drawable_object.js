c62c763a819e520385c03cb82a7dd7493e54ba11
eguitarz <dalema22@gmail.com>
2014-01-06 18:20:41 +0800

    remove old fn

--- js/drawable_object.js
+++ js/drawable_object.js
@@ -14,26 +14,6 @@ DrawableObject.prototype.paint = function(ctx){
 	ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
 	ctx.restore();
 }
-DrawableObject.prototype._paint = function(ctx, x, y, w, h){
-	// this.image.onload = function() {
-	setTimeout( function() {
-		ctx.save();
-		ctx.drawImage(this.image, x, y, w, h);
-		this.x = x;
-		this.y = y;
-		this.width = w;
-		this.height = h;
-		var d = ctx.getImageData(x, y, w, h);
-		for(var i=0; i<d.data.length; i+=4) {
-			if (d.data[i] === 255 && d.data[i+1] === 255 && d.data[i+2] === 255) {
-				d.data[i+3] = 0;
-			}
-		}
-		ctx.putImageData(d, x, y, 0, 0, w, h);
-		ctx.restore();
-	}.bind(this), 300);
-	// }.bind(this);
-};
 DrawableObject.prototype.loop = function(ctx, vx, vy){
 	ctx.save();
 	var a = setInterval( function() {
